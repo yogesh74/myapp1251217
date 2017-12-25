f5297357796eccbbc0d7b006dfdfde8883ce4be6
yogesh kumar <yogku74@yahoo.com>
2017-12-25 19:04:24 +0530

    251217

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public op:any="";
+  public res:any;
+  public a:number;
+  public b:number;
+  public Add(){
+    console.log(this.a);
+    console.log(typeof(this.a));
+    this.res="Add:"+(this.a+this.b);
+
+  }
+  public Add1(val:string){
+    console.log(this.a);
+    console.log(typeof(this.a));
+    this.res=val+":"+(this.a+this.b);
+
+  }
+  public Sub(){
+    console.log(this.a);
+    console.log(typeof(this.a));
+    this.res="Sub:"+(this.a-this.b);
+
+  }
+  public Sub1(val:string){
+    console.log(this.a);
+    console.log(typeof(this.a));
+    this.res=val+":"+(this.a-this.b);
+
+  }
+
 }
